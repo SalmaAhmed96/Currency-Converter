fccdc677dae051e9611acaeff90d73fbba8fe871
Salma <Salmaa_1781@hotmail.com>
2025-01-07 04:55:31 +0300

    Update

--- ChangeLogs.md
+++
@@ -1,5 +0,0 @@
-# Change Logs
-
-## Changes in Pull Request #1
-- Added new feature: Implemented currency conversion functionality in the `ConverterForm` component.
-- Updated `README.md` to include setup instructions and project overview.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-export default config;
+}
